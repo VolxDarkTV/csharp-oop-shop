8f5c1ed438dccc09746cc74f53e4a28c89ea9710
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-04-07 16:32:59 +0200

    Add project files.

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        //Attributi
+        public int codice;
+        public string nome;
+        public string descrizione;
+        public int prezzo;
+        public int prezzoIva;
+
+        //Costruttore
+        public Product()
+        {
+            codice = GenerateRandomNumber();
+            nome = "Prodotto";
+            descrizione = "Prodotto base";
+        }
+
+        public int GetCode()
+        {
+            return codice;
+        }
+
+        private int GenerateRandomNumber()
+        {
+            Random rnd = new Random();
+
+            //Console.WriteLine(rnd.Next());
+            return rnd.Next(10000000, 99999999);
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product();
+
+
+            //Genera codice casuale da 8 cifre
+
+
+
+            product1.codice = product1.GetCode();
+            int test = product1.codice;
+            Console.WriteLine(test);
+        }
+            
+
+    }
+}
